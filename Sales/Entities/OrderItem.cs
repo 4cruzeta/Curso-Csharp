c05b776231c557c1ffdb09f152d40d3fbc3dc0b3
4cruzeta <4cruzetam@gmail.com>
2019-11-20 12:41:26 -0300

    Mesma resalva de Date

---
+++ Sales/Entities/OrderItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization
+    ;
+namespace Sales.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;                                              // O preço aparece aqui também, além de constar
+            Product = product;                                          // no Product caso o preço do produto mude.
+        }                                                               // Ou seja, esse é o preço que foi vendido.
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return Product.Name
+                + ", $"
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", Quantity: "
+                + Quantity
+                + ", Subtotal: $"
+                + SubTotal().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
